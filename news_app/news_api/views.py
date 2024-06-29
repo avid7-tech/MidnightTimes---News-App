f00eb32cb02406caf5be3296a002ebedae02f261
avadhesh <avdheshojha7@gmail.com>
2024-06-29 22:22:03 +0530

    update sort

--- news_app/news_api/views.py
+++ news_app/news_api/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 import requests
 from datetime import datetime
 import logging
+from dotenv import load_dotenv
+import os
 
-API_KEY = '70a4cd3ca43d42dfae7423d7ee157628'
+
 logger = logging.getLogger(__name__)
-
+load_dotenv()
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 def home(request):
     keyword = request.GET.get('keyword')
     sort = request.GET.get('sort')
@@ -13,7 +16,7 @@ def home(request):
 
     if keyword:
         try:
-            url = f'https://newsapi.org/v2/everything?q={keyword}&from=2024-05-29&sortBy=publishedAt&apiKey={API_KEY}'
+            url = f'https://newsapi.org/v2/everything?q={keyword}&from=2024-05-29&sortBy=publishedAt&apiKey={NEWS_API_KEY}'
             response = requests.get(url)
             response.raise_for_status()  # Raise an exception for non-200 status codes
             data = response.json()
@@ -23,6 +26,8 @@ def home(request):
                 articles.sort(key=lambda x: datetime.strptime(x['publishedAt'][:10], '%Y-%m-%d'))
             elif sort == 'date_desc':
                 articles.sort(key=lambda x: datetime.strptime(x['publishedAt'][:10], '%Y-%m-%d'), reverse=True)
+                
+            context = {'articles': articles}
         
         except requests.exceptions.RequestException as e:
             # Handle errors like network issues or invalid API key
